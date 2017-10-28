39c3c44cedc84992918529b0bcd999becc8ea5a9
Jason De Lorme <jjdelorme@yahoo.com>
2017-10-28 19:55:38 -0400

    Organizing source into directories.

---
+++ AntMessageSimulator/Program/ApplicationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AntMessageSimulator
+{
+    public class ApplicationException : Exception
+    {
+        public ApplicationException(string message) : base(message)
+        {
+        }
+
+        public ApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
