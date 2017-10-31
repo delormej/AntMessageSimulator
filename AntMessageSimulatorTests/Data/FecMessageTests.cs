3aa3544e842d7cd7a12907eb965baa3037477c3a
Jason De Lorme <jjdelorme@yahoo.com>
2017-10-31 08:08:25 -0400

    Adding unit tests.

--- AntMessageSimulatorTests/Data/FecMessageTests.cs
+++ AntMessageSimulatorTests/Data/FecMessageTests.cs
@@ -11,14 +11,6 @@ namespace AntMessageSimulator.Tests
     [TestClass()]
     public class FecMessageTests
     {
-        DeviceSession session;
-
-        [TestInitialize]
-        public void Setup()
-        {
-            session = TestSetup.GetSessions()[6];
-        }
-
         [TestMethod()]
         public void GetSpecificTrainerDataTest()
         {
@@ -31,12 +23,19 @@ namespace AntMessageSimulator.Tests
         [TestMethod()]
         public void GetGeneralFeDataTest()
         {
-            Assert.Fail();
+            string line = "  4069.735 { 187267906} Rx - [A4][14][4E][02][10][19][00][00][00][00][FF][24][E0][89][E0][11][05][10][00][69][00][C0][C0][CA]";
+            Message message = Message.MessageFromLine(line);
+            var data = FecMessage.GetGeneralFeData(message);
+            float timestamp = GetDynamicObjectProperty<float>(data, "Timestamp");
+            float speed = GetDynamicObjectProperty<float>(data, "Speed");
+            Assert.IsTrue(timestamp == 4069.735F);
+            Assert.IsTrue(speed == 0.0F);
         }
 
         public static T GetDynamicObjectProperty<T>(object o, string property)
         {
-            return (T)o?.GetType().GetProperty(property)?.GetValue(o, null);
+            object value = o?.GetType().GetProperty(property)?.GetValue(o, null);
+            return (T)value;
         }
 
     }
