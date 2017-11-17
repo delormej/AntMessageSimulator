01a86caa28b51141b317b7070599cc4bcb5b10b2
Jason De Lorme <jjdelorme@yahoo.com>
2017-11-17 17:45:35 -0500

    Adding cloud storage and unit test.

---
+++ AntMessageSimulator/lib/CloudStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace AntMessageSimulator
+{
+    public class CloudStorage
+    {
+        CloudBlobContainer container;
+
+        public CloudStorage()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(GetConnectionString());
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            container = blobClient.GetContainerReference("rides");
+        }
+
+        public void Upload(string path)
+        {
+            try
+            {
+                string name = Path.GetFileName(path);
+                CloudBlockBlob blob = container.GetBlockBlobReference(name);
+                blob.DeleteIfExists();
+                blob.UploadFromFile(path);
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException("Unable to upload to Azure.", e);
+            }
+        }
+
+        private string GetConnectionString()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("CLOUD_STORAGE_ACCOUNT");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ApplicationException("Must set CLOUD_STORAGE_ACCOUNT environment variable.");
+            return connectionString;
+        }
+    }
+}
---
+++ AntMessageSimulatorTests/lib/CloudStorageTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AntMessageSimulator.Tests
+{
+    [TestClass()]
+    public class CloudStorageTests
+    {
+        const string dummyFile = @"..\..\dummy.txt";
+
+        [TestMethod()]
+        public void UploadTest()
+        {
+            CloudStorage storage = new CloudStorage();
+            CreateDummyFile(dummyFile);
+            storage.Upload(dummyFile);
+            File.Delete(dummyFile);
+        }
+
+        private void CreateDummyFile(string path)
+        {
+            File.WriteAllText(path, "dummy sample");
+        }
+    }
+}
