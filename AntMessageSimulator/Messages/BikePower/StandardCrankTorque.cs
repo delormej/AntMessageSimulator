4b05ccf742a852bd27316e8ada0cc511247ed34e
ms-jasondel <jasondel@microsoft.com>
2019-03-13 21:49:33 -0400

    Initial take at calcualting power.

--- AntMessageSimulator/Messages/BikePower/StandardCrankTorque.cs
+++ AntMessageSimulator/Messages/BikePower/StandardCrankTorque.cs
@@ -28,15 +28,71 @@ namespace AntMessageSimulator.Messages.BikePower
             var lastMessage = messages.FindLast(FindLastStandardCrankTorque) as StandardCrankTorque;
 
             if (lastMessage != null)
-                CalculatedPower = lastMessage.EventCount; // hack for now.
+            {
+                try
+                {
+                    InstantCadence = CalculateCadence(lastMessage);
+                    CalculatedPower = CalculatePower(lastMessage);
+                }
+                catch (Exception)
+                { }
+               
+            }
         }
 
         private bool FindLastStandardCrankTorque(Message message)
         {
+            //
+            // TODO: figure out how to pass context for averaging a certain # of seconds, instead of just using last message.
+            //
+
             StandardCrankTorque torqueMessage = message as StandardCrankTorque;
             if (torqueMessage == null)
                 return false;
             return (torqueMessage.EventCount< this.EventCount); // TODO: this is not accurate, because we need to deal with rollover.
+        }
+
+        private byte CalculateCadence(StandardCrankTorque lastMessage)
+        {
+            //
+            // TODO: this doesn't respect rollover math.
+            //
+            int cadence = 60 * (this.EventCount - lastMessage.EventCount) /
+                ( (this.Period - lastMessage.Period) / 2048 );
+
+            return (byte)cadence;
+        }
+
+        private ushort CalculatePower(StandardCrankTorque lastMessage)
+        {
+            double result = AverageAngularVelocity(lastMessage) * AverageAngularVelocity(lastMessage);
+            return (ushort)result;
+        }
+
+        private double AverageAngularVelocity(StandardCrankTorque lastMessage)
+        {
+            //
+            // TODO: this doesn't respect rollover math.
+            //
+            double result = ( 2 * Math.PI * (this.EventCount - lastMessage.EventCount) ) / 
+                ( (this.Period - lastMessage.Period) / 2048 );
+
+            return result;
+        }
+
+        private double AverageTorque(StandardCrankTorque lastMessage)
+        {
+            double result = (this.AccumulatedTorque - lastMessage.AccumulatedTorque) / 
+                (32 * (this.EventCount - lastMessage.EventCount));
+
+            return result;
+        }
+
+        private double AveragePower(double averageTorque, double angularVelocity)
+        {
+            return averageTorque * angularVelocity;
+
+            // OR Power = 128 * Math.PI * ( (AccumulatedTorque - last.AccumulatedTorque) / (Period - last.Period) )
         }
     }
 }
