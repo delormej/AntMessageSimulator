ddb902e8043dbed283690030fd34d6bd13d2c47e
jjdelorme <jjdelorme@yahoo.com>
2020-01-07 09:28:17 -0500

    Added a message type for Irt Settings to read servo offset, etc...

---
+++ AntMessageSimulator/Messages/Fec/IrtSettingsMessage.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AntMessageSimulator.Messages.Fec
+{
+    public class IrtSettingsMessage : Message
+    {
+        public new const byte Page = 0xF2;
+
+        public IrtSettingsMessage(Message message) : base(message)
+        {
+            if (message.MessageId != Page)
+                throw new ApplicationException("Not a valid Irt Settings message.");
+
+            Drag = Decode(message, 1);
+            RollingResistance = Decode(message, 3);
+            ServoOffset = Decode(message, 5);
+            Settings = message.Bytes[7];
+        }
+
+        public ushort Drag { get; private set; }
+        public ushort RollingResistance{ get; private set; }
+        public ushort ServoOffset { get; private set; }
+        public byte Settings{ get; private set; }
+
+        private static ushort Decode(Message message, int offset)
+        {
+            return (ushort)(message.Bytes[offset + MESSAGE_HEADER_LENGTH] |
+                (message.Bytes[offset + 1 + MESSAGE_HEADER_LENGTH] << 8));
+        }
+
+
+        public override string ToString()
+        {
+            const string format = "{{ Timestamp = {0:F3}, Drag = {1:G}, RollingResistance = {2:G}, ServoOffset = {3:G}, Settings = {4} }}";
+            return string.Format(format,
+                Timestamp,
+                Drag,
+                RollingResistance,
+                ServoOffset,
+                Settings);
+        }
+    }
+}
---
+++ AntMessageSimulatorTests/Messages/IrtSettingsMessageTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AntMessageSimulator;
+using System;
+using System.Collections.Generic;
+using AntMessageSimulator.Messages.Fec;
+
+namespace AntMessageSimulator.Tests
+{
+    [TestClass()]
+    public class IrtSettingsMessageTest
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            const string line = "    34.562 {   1133515} Rx - [A4][0E][4E][01][F2][56][5F][CF][46][B4][00][FF][80][8E][F3][11][05][35]";
+            IrtSettingsMessage message = MessageFactory.MessageFromLine(line) as IrtSettingsMessage;
+            Assert.IsNotNull(message);
+            Assert.IsTrue(message.ServoOffset == 180);
+        }
+    }
+}
