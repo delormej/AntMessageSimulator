de0c82c307d3a90b64449856c3c360b27afb895f
Jason De Lorme <delormej@gmail.com>
2020-12-04 09:51:18 -0500

    Missing files.

---
+++ AntMessageSimulator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AntMessageSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer.Welcome();
+            try
+            {
+                ExecutionOptions options = new ExecutionOptions(args);
+                Simulator simulator = new Simulator(options);
+                simulator.Execute();
+            }
+            catch (Exception e)
+            {
+                Printer.Error(e.Message);
+                Printer.Usage();
+            }
+        }
+    }
+}
